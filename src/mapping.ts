94c819c03d1f17aed4bde8fd97fac582960723cc
kumailnaqvi354 <komailnaqvi616@gmail.com>
2022-03-11 21:24:44 +0500

    init

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,53 +1,62 @@
-import {
-  Approval as ApprovalEvent,
-  ApprovalForAll as ApprovalForAllEvent,
-  OwnershipTransferred as OwnershipTransferredEvent,
-  Transfer as TransferEvent
-} from "../generated/Token/Token"
-import {
-  Approval,
-  ApprovalForAll,
-  OwnershipTransferred,
-  Transfer
-} from "../generated/schema"
+import {Transfer as TransferEvent, Token as TokenContract} from '../generated/Token/Token';
+import {Token,User} from '../generated/schema';
+import { ipfs, json } from "@graphprotocol/graph-ts";
 
-export function handleApproval(event: ApprovalEvent): void {
-  let entity = new Approval(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.owner = event.params.owner
-  entity.approved = event.params.approved
-  entity.tokenId = event.params.tokenId
-  entity.save()
-}
+const ipfsHash = 'QmSr3vdMuP2fSxWD7S26KzzBWcAN1eNhm4hk1qaR3x3vmj';
 
-export function handleApprovalForAll(event: ApprovalForAllEvent): void {
-  let entity = new ApprovalForAll(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.owner = event.params.owner
-  entity.operator = event.params.operator
-  entity.approved = event.params.approved
-  entity.save()
-}
+export function handleTransfer(event: TransferEvent): void{
+  let token  =  Token.load(event.params.tokenId.toString());
 
-export function handleOwnershipTransferred(
-  event: OwnershipTransferredEvent
-): void {
-  let entity = new OwnershipTransferred(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.previousOwner = event.params.previousOwner
-  entity.newOwner = event.params.newOwner
-  entity.save()
-}
+  if(!token){
+    token = new Token(event.params.tokenId.toString());
+    token.tokenID =  event.params.tokenId;
 
-export function handleTransfer(event: TransferEvent): void {
-  let entity = new Transfer(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.from = event.params.from
-  entity.to = event.params.to
-  entity.tokenId = event.params.tokenId
-  entity.save()
-}
+    token.tokenURI = "/" + event.params.tokenId.toString() + '.json';
+
+    let  metadata = ipfs.cat(ipfsHash + token.tokenURI);
+
+    if (metadata) {
+      const value = json.fromBytes(metadata).toObject();
+      if(value){
+        const image = value.get('image');
+        const name = value.get('name');
+        const description = value.get('description');
+        const externalURI = value.get('external_url');
+        if (name && image && description && externalURI) {
+            token.image = image.toString();
+            token.description = description.toString();
+            token.name = name.toString();
+            token.externalURL = externalURI.toString();
+            token.ipfsURI = 'ipfs.io/ipfs' + ipfsHash + token.tokenURI;
+          }
+          const coven = value.get('coven');
+            if(coven){
+              let covenData = coven.toObject();
+              const type = covenData.get('type');
+              if(type){
+                token.type = type.toString();
+              } 
+              const birthChart = covenData.get('birthChart');
+              if(birthChart){
+                let birthChartData = birthChart.toObject();
+                const sun = birthChartData.get('sun');
+                const moon = birthChartData.get('moon');
+                const rising = birthChartData.get('rising');
+              if(sun && moon && rising){
+                token.sun = sun.toString();
+                token.moon = moon.toString();
+                token.rising = rising.toString();
+              }
+              }
+              
+            }
+
+
+      }
+    }
+  }
+
+  token.updatedAtTimeStamp = event.block.timestamp;
+  token.owner = event.params.to.toHexString();
+  token.save();
+  }
